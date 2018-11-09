6731f6dc38065fdf7c9c6bb24750ca964452f2e1
Austen Grice-Griffin <austen.grice-griffin@Austens-MBP.home>
2018-11-09 17:26:06 +0000

    adds and withdraws funds

--- tech-test/spec/BankSpec.js
+++ tech-test/spec/BankSpec.js
@@ -9,4 +9,15 @@ describe('Balance', function() {
    it('starts at 100 ', function() {
      expect(bank.getCurrentBalance()).toEqual(100);
    });
+
+   it('Adds funds (10)', function() {
+     bank.deposit(10);
+     expect(bank.getCurrentBalance()).toEqual(110);
+   });
+
+   it('Withdraws funds (10)', function() {
+     bank.withdrawl(10);
+     expect(bank.getCurrentBalance()).toEqual(90);
+   });
+
  });
--- tech-test/src/Bank.js
+++ tech-test/src/Bank.js
@@ -2,9 +2,17 @@
 
 function Bank() {
    this.balance = 100;
-  
+
  }
 
  Bank.prototype.getCurrentBalance = function() {
     return this.balance;
   };
+
+  Bank.prototype.deposit = function(value) {
+     this.balance += value;
+   }
+
+   Bank.prototype.withdrawl = function(value) {
+     this.balance -= value;
+   }
