7257f395d0c27fe31bb923735659c4a48b635fc8
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-15 17:01:25 +0000

    withdraw

--- tech-test/JS/jquery.js
+++ tech-test/JS/jquery.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
   var bank = new Bank();
 
-$('#Balance').click(function(){
-  $('#currentbal').text(bank.balance + " GBP");
-});
+  $('#Balance').click(function(){
+    $('#currentbal').text(bank.balance + " GBP");
+  });
 
-$('#Deposit').click(function(){
-  var amount = $('#value').val();
-  var integer = parseInt(amount, 10); // converts string to integer (base 10)
-  bank.deposit(integer);
-  $('#currentbal').text(bank.balance + " GBP");
-});
+  $('#Deposit').click(function(){
+    var amount = $('#value').val();
+    var integer = parseInt(amount, 10); // converts string to integer (base 10)
+    bank.deposit(integer);
+    $('#currentbal').text(bank.balance + " GBP");
+  });
 
-
+  $('#Withdraw').click(function(){
+    var amount = $('#value').val();
+    var integer = parseInt(amount, 10); // converts string to integer (base 10)
+    bank.withdrawal(integer);
+    $('#currentbal').text(bank.balance + " GBP");
+  });
 
 });
