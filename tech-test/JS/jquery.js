6f697677b1f21045259c3d935afa9c6d7864b0c9
Austen Grice-Griffin <austen.grice-griffin@Austens-MacBook-Pro.local>
2018-11-15 17:21:26 +0000

    statement alert

--- tech-test/JS/jquery.js
+++ tech-test/JS/jquery.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   });
 
   $('#Deposit').click(function(){
+    bank.addTransactions("Deposit")
     var amount = $('#value').val();
     var integer = parseInt(amount, 10); // converts string to integer (base 10)
     bank.deposit(integer);
@@ -13,10 +14,15 @@ $(document).ready(function() {
   });
 
   $('#Withdraw').click(function(){
+    bank.addTransactions("Withdraw")
     var amount = $('#value').val();
     var integer = parseInt(amount, 10); // converts string to integer (base 10)
     bank.withdrawal(integer);
     $('#currentbal').text(bank.balance + " GBP");
+  });
+
+  $('#Statement').click(function(){
+    alert(bank.Transactions);
   });
 
 });
